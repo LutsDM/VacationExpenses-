91e8bb682c6a77fd0110a30c6efb8e3315957051
SergejL-code <skylerus@mail.de>
2024-11-18 15:20:45 +0100

    test2

--- vacationExpenses/code/VacationExpenses/test/ExpenseTest1.java
+++ vacationExpenses/code/VacationExpenses/test/ExpenseTest1.java
@@ -1,7 +1,11 @@
 package VacationExpenses.test;
 
+import java.util.ArrayList;
+
 public class ExpenseTest1 {
 
-
+/ Конструктор, который загружает данные при инициализации
+        public FileExpenseDAO() {  expenses = new ArrayList<>();
+        }
 
 }
